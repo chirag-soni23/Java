23e8cb760b6edaa185006eade0f93c01136c69c0
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-02-29 08:12:38 +0530

    Java

---
+++ Challenge2/Primenumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Primenumber {
+       public static void main(String[] args) {
+              @SuppressWarnings("resource")
+              Scanner input = new Scanner(System.in);
+              System.out.println("Welcome to prime number");
+              System.out.print("Please, Enter a number: ");
+              int num = input.nextInt();
+              boolean primenum = primenumber(num);
+              System.out.println("The result is : " +  primenum);
+       }  
+       public static boolean primenumber(int num){
+              int i = 2;
+              while(i < num){
+                     if(num % i == 0){
+                            return false;
+                     }
+                     i++;
+              }
+              return true;
+       }     
+}
