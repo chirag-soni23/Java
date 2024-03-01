e9ac4454079efe701e3c5a4cea320154bf7d24ec
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-01 17:26:07 +0530

    Java

---
+++ Arrays/Array.java
@@ -0,0 +1,16 @@
+public class Array{
+       public static void main(String[] args) {
+              System.out.println("Welcome to the array");
+              int[] myArr = {1,2,3,4,5,6,7,8,9,10};
+              // System.out.println(myArr[0]);
+              // System.out.println(myArr.length);
+
+              // Array Travesal
+              int i = 0;
+              while(i < myArr.length){
+                     System.out.print(myArr[i] + " ");
+                     i++;
+
+              }
+       }
+}
