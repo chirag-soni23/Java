e8f0b552a6d77b7f011195e76bab3e4f31252dbd
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-03 19:02:20 +0530

    Java

--- Challenge3/Arrayutility.java
+++ Challenge3/Arrayutility.java
@@ -17,6 +17,27 @@ public class Arrayutility{
 
        }
 
+       public static int[][] input2DArray(){
+              @SuppressWarnings("resource")
+              Scanner input = new Scanner(System.in);
+              System.out.println("Please, Enter a row: ");
+              int row = input.nextInt();
+              System.out.println("Please, Enter a column: ");
+              int column = input.nextInt();
+              int[][] numArr = new int[row][column];
+              int i = 0;
+              while(i<row){
+                     int j = 0;
+                     while (i<column) {
+                            System.out.print("Please enter element roes : " + (i + 1) + ",  column: "  + (j+1) + " : ");
+                            j++;
+                            
+                     }
+                     i++;
+              }
+              return numArr;
+       }
+
        public static void displayArr(int[] numArr){
               int i = 0;
               while(i<numArr.length){
