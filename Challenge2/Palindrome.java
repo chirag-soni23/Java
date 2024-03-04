870477adf7b730f933d056b697d88ec80047d862
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-04 17:12:55 +0530

    Java

--- Challenge2/Palindrome.java
+++ Challenge2/Palindrome.java
@@ -8,30 +8,29 @@ public class Palindrome {
               System.out.print("Please enter a number: ");
               int num = input.nextInt();
               boolean palindrome = isPalindrome(num);
-              if(palindrome){
+              if (palindrome) {
                      System.out.println("Yes, it is a palindrome number");
-              }
-              else{
+              } else {
                      System.out.println("No, it is not a palindrome number");
               }
 
        }
-       public static boolean isPalindrome(int num){
-                     int reverse = reverse(num);
-                     return num == reverse;
-              
-          
-       }
-              public static int reverse(int num){
-                     int newNum = 0;
-                     while(num > 0){
-                            int digit = num % 10;
-                            newNum = (newNum * 10) + digit;
-                            num /= 10;
-       
-                     }
-       
-                     return newNum;
-              }   
+
+       public static boolean isPalindrome(int num) {
+              int reverse = reverse(num);
+              return num == reverse;
+
        }
 
+       public static int reverse(int num) {
+              int newNum = 0;
+              while (num > 0) {
+                     int digit = num % 10;
+                     newNum = (newNum * 10) + digit;
+                     num /= 10;
+
+              }
+
+              return newNum;
+       }
+}
