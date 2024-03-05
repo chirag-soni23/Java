6ec372f8f93c6e6254311df88102fe8b9d27b333
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-05 17:46:23 +0530

    Java

--- Challenge3/NumberofOccurance.java
+++ Challenge3/NumberofOccurance.java
@@ -8,18 +8,18 @@ public class NumberofOccurance {
               int[] numArr = Arrayutility.inputArray();
               System.out.print("Now enter a value you want to find: ");
               int num = input.nextInt();
-              int occurance = noOfOccurance(numArr,num);
+              int occurance = noOfOccurance(numArr, num);
               System.out.println("The number of occurance are found in " + occurance);
        }
-       public static int noOfOccurance(int[] numArr,int num){
+
+       public static int noOfOccurance(int[] numArr, int num) {
               int occ = 0;
               int i = 0;
-              while(i<numArr.length){
-                     if(numArr[i] == num){
+              while (i < numArr.length) {
+                     if (numArr[i] == num) {
                             occ++;
                      }
                      i++;
-
 
               }
               return occ;
