22349fdda8963ea553e63921cc75eab95183995e
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-05 17:45:37 +0530

    Java

--- Challenge3/MinMaxArr.java
+++ Challenge3/MinMaxArr.java
@@ -1,4 +1,4 @@
-public class MinMaxArr{
+public class MinMaxArr {
        public static void main(String[] args) {
               System.out.println("Welcome to min and max array");
               int[] numArr = Arrayutility.inputArray();
@@ -7,15 +7,16 @@ public class MinMaxArr{
               System.out.println("The Maximum Value is: " + max);
               System.out.println("The Minimum Value is: " + min);
        }
+
        // Max Value
-       public static int maxValue(int[] numArr){
-              if(numArr.length == 0){
+       public static int maxValue(int[] numArr) {
+              if (numArr.length == 0) {
                      return Integer.MIN_VALUE;
               }
               int max = numArr[0];
               int i = 1;
-              while(i<numArr.length){
-                     if(max < numArr[i]){
+              while (i < numArr.length) {
+                     if (max < numArr[i]) {
                             max = numArr[i];
 
                      }
@@ -24,12 +25,13 @@ public class MinMaxArr{
               return max;
 
        }
+
        // Min Value
-       public static int minValue(int[] numArr){
+       public static int minValue(int[] numArr) {
               int min = Integer.MAX_VALUE;
               int i = 0;
-              while(i<numArr.length){
-                     if(min > numArr[i]){
+              while (i < numArr.length) {
+                     if (min > numArr[i]) {
                             min = numArr[i];
                      }
                      i++;
