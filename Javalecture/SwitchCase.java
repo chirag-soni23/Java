b23ad7c426e122a2de73ca5dbbf8623621002e92
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-06 09:22:19 +0530

    Java

--- Javalecture/SwitchCase.java
+++ Javalecture/SwitchCase.java
@@ -7,38 +7,55 @@ public class SwitchCase {
               System.out.println("Welcome to switch case");
               System.out.print("Enter your day in number: ");
               int day = input.nextInt();
-              oldSwitch(day);
+              // oldSwitch(day);
+              newSwitch(day);
 
-       }       
+       }
 
-       public static void oldSwitch(int day){
-              switch(day){
+       // New switch case.
+       public static void newSwitch(int day) {
+              String output = switch (day) {
+                     case 1 -> "Monday";
+                     case 2 -> "Tuesday";
+                     case 3 -> "Wednesday";
+                     case 4 -> "Thrusday";
+                     case 5 -> "Friday";
+                     case 6 -> "Saturday";
+                     case 7 -> "Sunday";
+                     default -> "Invalid days";
+              };
+              System.out.println(output);
+       }
+
+       // Old switch case.
+       public static void oldSwitch(int day) {
+              switch (day) {
                      case 1:
-                     System.out.println("Monday");
-                     break;
+                            System.out.println("Monday");
+                            break;
                      case 2:
-                     System.out.println("Tuesday");
-                     break;
+                            System.out.println("Tuesday");
+                            break;
                      case 3:
-                     System.out.println("Wednesday");
-                     break;
+                            System.out.println("Wednesday");
+                            break;
                      case 4:
-                     System.out.println("Thrusday");
-                     break;
+                            System.out.println("Thrusday");
+                            break;
                      case 5:
-                     System.out.println("Friday");
-                     break;
+                            System.out.println("Friday");
+                            break;
                      case 6:
-                     System.out.println("Saturday");
-                     break;
+                            System.out.println("Saturday");
+                            break;
                      case 7:
-                     System.out.println("Sunday");
-                     break;
+                            System.out.println("Sunday");
+                            break;
                      default:
-                     System.out.println("Invalid days");
-                     break;
+                            System.out.println("Invalid days");
+                            break;
               }
 
        }
 
-       }
+}
