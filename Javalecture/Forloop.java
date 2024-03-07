a6ba8abe39ff2ef4617fdb0a6c84bdaa7e3f2009
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-07 07:44:08 +0530

    Java

---
+++ Javalecture/Forloop.java
@@ -0,0 +1,17 @@
+public class Forloop {
+       public static void main(String[] args) {
+              // While loop
+              // int i = 1;
+              // while(i<=10){
+              //        System.out.println(i + " ");
+              //        i++;
+
+              // }
+
+              // for loop
+              for(int i = 1; i<=10;i++){
+                     System.out.println(i + " ");
+              }
+
+       }
+}
