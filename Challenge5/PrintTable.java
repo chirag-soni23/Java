6b26df2f275393c8416da34643367c6fe37b69b5
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-07 15:43:12 +0530

    Java

---
+++ Challenge5/PrintTable.java
@@ -0,0 +1,15 @@
+package Challenge5;
+
+import java.util.Scanner;
+public class PrintTable{
+       public static void main(String[] args) {
+              @SuppressWarnings("resource")
+              Scanner input = new Scanner(System.in);
+              System.out.println("Welcome to Multiplicatio Table");
+              System.out.print("Please, Enter a number if you  want to print table: ");
+              int num = input.nextInt();
+              for(int i = 1; i<=10;i++){
+                     System.out.println(num + " x " + i + " = " + (num*i));
+              }
+       }
+}
