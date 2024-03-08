5e943bb5cd5dc622cdda1210d8a30366dd91f28f
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-08 18:55:09 +0530

    Java

---
+++ Javalecture/Stringbuilder.java
@@ -0,0 +1,11 @@
+public class Stringbuilder{
+
+       public static void main(String[] args) {
+              StringBuilder sentence = new StringBuilder("This is a Sentence");
+              sentence.append(" Added me. ");
+              sentence.append("now");
+              System.out.println(sentence);
+
+       }
+
+}
