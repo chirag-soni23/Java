2b5dd6aeb093965fd9134a3d03a34e91b1e8bc45
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-09 08:32:21 +0530

    Java

---
+++ Challenge6/StringManipulation.java
@@ -0,0 +1,10 @@
+package Challenge6;
+
+public class StringManipulation {
+       public static void main(String[] args) {
+              String firstName = "Chirag";
+              String lastName = "Soni";
+              String fullName = firstName.concat(" ").concat(lastName);
+              System.out.println(fullName);
+       }
+}
