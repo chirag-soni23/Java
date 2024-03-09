74469b48c1885c4a2a9d2a0fc6f0914ec397d06d
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-09 10:17:20 +0530

    Java

---
+++ Challenge6/Carfinal.java
@@ -0,0 +1,14 @@
+package Challenge6;
+public class Carfinal{
+      final int maxSpeed;
+      final String name;
+      final String model;
+       public Carfinal(int maxSpeed, String name, String model) {
+              this.maxSpeed = maxSpeed;
+              this.name = name;
+              this.model = model;
+              // this.maxSpeed++;
+       }
+       
+       
+}
