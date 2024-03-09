383d785e61730127f98e1b21f6b258d6010a7334
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-09 07:52:07 +0530

    Java

---
+++ Challenge5/Final.java
@@ -0,0 +1,7 @@
+public class Final {
+       public static void main(String[] args) {
+              final double PI = 3.14159265359;
+              // PI = 2;
+              System.out.println(PI);
+       }
+}
