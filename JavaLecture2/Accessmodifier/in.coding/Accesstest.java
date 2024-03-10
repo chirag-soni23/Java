a5d691276b43c89d126e72c8cf39b5527a351427
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-10 10:17:27 +0530

    Java

---
+++ JavaLecture2/Accessmodifier/in.coding/Accesstest.java
@@ -0,0 +1,12 @@
+public class Accesstest {
+       public static void main(String[] args) {
+              AccessCar myCar = new AccessCar("Swift","s34rrd",5000,120,"Chirag");
+
+              /**
+               * price not show in suggestion because AccessCar.java file in price method are in private.
+               */
+              // System.out.println(myCar.price);
+              System.out.println(myCar.name);
+          }
+       
+}
---
+++ JavaLecture2/Accessmodifier/in.coding/Defaulttest.java
@@ -0,0 +1,9 @@
+public class Defaulttest {
+       public static void main(String[] args) {
+              AccessCar myCar = new AccessCar("bmw","x56",7000,200,"Kartik");
+              System.out.println(myCar.owner);
+
+
+       }
+       
+}
