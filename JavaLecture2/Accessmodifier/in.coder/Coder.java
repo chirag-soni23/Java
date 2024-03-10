82009433b976562f1057a99463b01d583a93f2ac
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-10 11:04:52 +0530

    Java

--- JavaLecture2/Accessmodifier/in.coder/Coder.java
+++ JavaLecture2/Accessmodifier/in.coder/Coder.java
@@ -1,9 +1,10 @@
+
 public class Coder {
        public static void main(String[] args) {
               AccessCar myCar = new AccessCar("mercedes", "31ws", 10000, 180,
               "Abhishek");
               System.out.println(myCar.owner);
-       }
+       }   
        
        
 }
---
+++ JavaLecture2/GetterandSetter/GetterandSetter.java
@@ -0,0 +1,8 @@
+public class GetterandSetter {
+       public static void main(String[] args) {
+              Cycle myCycle = new Cycle(2, 30, "Red");
+              myCycle.setColor("Blue");
+       
+              System.out.println(myCycle.getColor());
+       }
+}
