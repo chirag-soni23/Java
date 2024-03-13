4028701b46aa0af61963318d14a288332e27d05a
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-13 08:20:31 +0530

    Java

---
+++ Javalecture3/Abstraction/Testabstraction.java
@@ -0,0 +1,9 @@
+public class Testabstraction {
+       public static void main(String[] args) {
+              // Vehicle vehicle = new Vehicle(4);
+              Car car = new Car();
+              car.commute();
+              car.getSetgo();
+
+       }
+}
---
+++ Javalecture3/Abstraction/Vehicle.java
@@ -0,0 +1,23 @@
+public abstract class Vehicle implements Transport{
+       @SuppressWarnings("unused")
+
+       private int noOfWheels;
+
+       public Vehicle(int noOfWheels) {
+              this.noOfWheels = noOfWheels;
+       }
+
+       @Override
+       public void getSetgo() {
+              System.out.println("Get set go");
+       }
+
+
+       public void commute(){
+              System.out.println("I am a vehicle");
+       }
+
+       
+
+
+}
