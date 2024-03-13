8147e3ea9ca3a0946e0ec76b3fad8b4744588160
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-14 04:36:28 +0530

    Java

---
+++ Javalecture3/Overloading.java
@@ -0,0 +1,9 @@
+public class Overloading{
+       public int add(int a, int b){
+              return a + b;
+       }
+       public int add(int a, int b, int c, int d){
+              return a + b + c + d;
+       }
+
+}
