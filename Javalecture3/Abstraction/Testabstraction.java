1eabe0a57862c8bf11c782dae1600e3707637ef7
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-16 08:23:00 +0530

    Java

--- Javalecture3/Abstraction/Testabstraction.java
+++ Javalecture3/Abstraction/Testabstraction.java
@@ -1,9 +1,9 @@
 public class Testabstraction {
        public static void main(String[] args) {
               // Vehicle vehicle = new Vehicle(4);
-              CarPoly car = new CarPoly();
-              car.commute();
-              car.getSetgo();
+              // CarPoly car = new CarPoly();
+              // car.commute();
+              // car.getSetgo();
 
        }
 }
