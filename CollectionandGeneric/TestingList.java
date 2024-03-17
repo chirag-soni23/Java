5a14b46d08e67bc7c2b299f802f33db9aa4591cc
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-17 08:44:38 +0530

    Java

---
+++ CollectionandGeneric/TestingList.java
@@ -0,0 +1,23 @@
+package CollectionandGeneric;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestingList{
+       public static void main(String[] args) {
+              List<String> list = new ArrayList<>();
+              System.out.println(list.add("Chirag"));
+              System.out.println(list.add("Soni"));
+
+              if(list.contains("Soni")){
+                     System.out.println(list.indexOf("Soni"));
+              }
+
+              for (int i = 0; i < list.size(); i++) {
+                     System.out.println(list.get(i)); 
+              }
+             for (String lists : list) {
+              System.out.println(lists);
+             };
+       }
+       
+}
