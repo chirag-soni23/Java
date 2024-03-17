56706f9201c9b329447de39159da6180f2ef7a18
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-17 09:29:36 +0530

    Javaa

--- Challenge2/Armstrongnumber.java
+++ Challenge2/Armstrongnumber.java
@@ -8,31 +8,30 @@ public class Armstrongnumber {
               System.out.print("Please, Enter a number: ");
               int num = input.nextInt();
               boolean arms = isarmstrong(num);
-              if(arms){
+              if (arms) {
                      System.out.println("Yes, it is a armstrong number.");
-              }
-              else{
+              } else {
                      System.out.println("Not, it is not a armstrong number.");
               }
-              
 
        }
-       public static boolean isarmstrong(int num){
+
+       public static boolean isarmstrong(int num) {
               int digit = noOfdigit(num);
               int originalNum = num;
               int finalNumber = 0;
-              while(num > 0){
+              while (num > 0) {
                      int lastDigit = num % 10;
                      num /= 10;
-                     finalNumber += pow(lastDigit,digit);
+                     finalNumber += pow(lastDigit, digit);
               }
               return finalNumber == originalNum;
        }
 
-       public static int pow(int num1, int num2){
+       public static int pow(int num1, int num2) {
               int result = 1;
               int i = 0;
-              while(i < num2){
+              while (i < num2) {
                      result *= num1;
                      i++;
 
@@ -40,11 +39,9 @@ public class Armstrongnumber {
               return result;
        }
 
-
-
-       public static int noOfdigit(int num){
+       public static int noOfdigit(int num) {
               int digit = 0;
-              while(num > 0){
+              while (num > 0) {
                      digit++;
                      num /= 10;
               }
