5862a1f5240c5d0be50ca663d8aac8bb24efcc20
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-17 15:12:24 +0530

    Java

--- Challenge10/Calculator.java
+++ Challenge10/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator{
        public static void main(String[] args) {
               Calculator calc = new Calculator();
               System.out.println(calc.add(3, 14));
-              System.out.println(calc.add(3, 5,4));
+              System.out.println(calc.add(3,5,4));
               System.out.println(calc.add(12.2, 13.2));
        }
 }
