ced94944f78199d6ed574406e292024d0238e66d
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-03-22 08:57:57 +0530

    Java

--- FunctionalProgramming/TestingReduce.java
+++ FunctionalProgramming/TestingReduce.java
@@ -1,6 +1,26 @@
 package FunctionalProgramming;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class TestingReduce {
        public static void main(String[] args) {
-             
+              List<Integer> number = List.of(1, 2, 3, 4, 5);
+              int newSum = number.stream().reduce(0, new BinaryOperator<Integer>() {
+                     @Override
+                     public Integer apply(Integer integer1, Integer integer2) {
+                            return integer1 + integer2;
+                     }
+              });
+              System.out.println(newSum);
+
+              System.out.println("Printing reduce method");
+              int newSum2 = number.stream().reduce(0, (a,b)->a+b);
+              System.out.println(newSum2);
+
+              System.out.println("Get max value");
+              int max = number.stream().reduce(Integer.MIN_VALUE,(a,b)-> a>b ? a:b);
+              System.out.println(max);
+
        }
 }
